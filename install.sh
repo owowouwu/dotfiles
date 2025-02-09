04c9d5bba02e55335bc8a8ae89daf5410d4cc89a
Steven Nguyen <steven.n.1618@gmail.com>
2025-02-09 12:16:57 +1100

    add ghostty and other stuffs

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env interpreter
 
 # setup
-packages = (fzf zsh neovim tmux kitty )
-sudo dnf install fzf 
+sudo dnf copr enable atim/lazygit -y
+packages = (fzf zsh neovim tmux kitty lazygit nnn)
+sudo dnf install packages 
 
 curl -sS https://starship.rs/install.sh | sh
+curl -sS Anaconda3-2024.10-1-Linux-x86_64.sh | sh
